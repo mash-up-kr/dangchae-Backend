36b5c8b04e2e21146f330fa4f9ffcb1ce8216b8b
Yangsiyoung <ysjleader@gmail.com>
2021-02-27 21:19:00 +0900

    Spring Security 적용 JWT 토큰 기반 인증

---
+++ src/main/java/kr/mashup/udada/config/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,54 @@
+package kr.mashup.udada.config.jwt;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.GenericFilterBean;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends GenericFilterBean {
+
+    private final JwtProvider jwtProvider;
+
+    /**
+     * 보통 JWT토큰은 header에 담아서 요청을 보냄
+     * 서버는 헤더를 까서 올바른 JWT를 획득하고
+     * 그다음 아래 2가지 수행
+     *
+     * 1. JWT Validation Check
+     * 2. JWT 가 올바르다면 Authentication 객체 SecurityContextHolder 에 넣어줘야해
+     * 그럼 UsernamePasswordAuthtenticationFilter 안타니까
+     *
+     * filter1 -> filter2 -> filter3 ... FilterChaining
+     * FilterChain.doFilter(request, response)
+     *
+     * Request -> 1 times
+     * Response -> 1 times
+     */
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        System.out.println(((HttpServletRequest)request).getRequestURL());
+        String token = jwtProvider.getTokenFromHeader(((HttpServletRequest) request));
+        if (token != null && jwtProvider.validateTokenIssuedDate(token)) {
+            Authentication authentication = jwtProvider.getAuthentication(token);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        } else {
+            HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+            httpServletResponse.setStatus(400);
+            httpServletResponse.setContentType("application/json");
+            httpServletResponse.setCharacterEncoding("utf8");
+            httpServletResponse.getWriter().write("비정상 메시지");
+            return;
+        }
+
+        chain.doFilter(request, response);
+    }
+}
---
+++ src/main/java/kr/mashup/udada/config/jwt/JwtProvider.java
@@ -0,0 +1,72 @@
+package kr.mashup.udada.config.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Base64;
+import java.util.Date;
+
+@RequiredArgsConstructor
+@Component
+public class JwtProvider {
+
+    /**
+     * 1. JWT 토큰 생성
+     * 2. JWT 토큰 Validation Check
+     * 3. Authentication 객체 생성
+     */
+    @Value("${jwt.secret-key}")
+    private String secretKey;
+
+    private long tokenValidTime = 30 * 24 * 60 * 60 * 1000L;
+
+    public static final String HEADER_NAME = "Authorization";
+
+    private final UserDetailsService userDetailsService;
+
+    @PostConstruct
+    protected void init() {
+        secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+    }
+
+    public String createToken(String sub) {
+        Claims claims = Jwts.claims().setSubject(sub);
+        Date date = new Date();
+        return Jwts.builder().setClaims(claims).setIssuedAt(date).setExpiration(new Date(date.getTime() + tokenValidTime))
+                .signWith(SignatureAlgorithm.HS256, secretKey).compact();
+    }
+
+    public Authentication getAuthentication(String token) {
+        UserDetails userDetails = userDetailsService.loadUserByUsername(getUserSub(token));
+        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+    }
+
+    public String getUserSub(String token) {
+        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public String getTokenFromHeader(HttpServletRequest request) {
+        return request.getHeader(HEADER_NAME).replace("Bearer", "").trim();
+    }
+
+    public boolean validateTokenIssuedDate(String token) {
+        try {
+            Jws<Claims> claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
+            return !claims.getBody().getExpiration().before(new Date());
+        }catch (Exception e) {
+            return false;
+        }
+    }
+}
