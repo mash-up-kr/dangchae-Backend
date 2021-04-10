5b19972a094cc74b89396b98c7238f56bea4f780
ParkMiHyun <mihyunpark24@gmail.com>
2021-04-10 21:11:04 +0900

    fix: 복수 다이어리 get 오류 해결

--- src/main/java/kr/mashup/udada/diary/dao/DiaryRepository.java
+++ src/main/java/kr/mashup/udada/diary/dao/DiaryRepository.java
@@ -2,15 +2,10 @@ package kr.mashup.udada.diary.dao;
 
 import kr.mashup.udada.diary.domain.Diary;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface DiaryRepository extends JpaRepository<Diary, Long> {
 
-    @Query(nativeQuery = true,
-            value = "SELECT * " +
-                    "FROM diary " +
-                    "WHERE diary_id = (SELECT diary_id FROM diary_user WHERE user_id = ?1)")
     List<Diary> findByUserId(Long userId);
 }
